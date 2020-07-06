61dc99ea5a60ed23b81fffddc1c9d08eb8b10787
lijing <muclxylijing188@126.com>
2020-07-06 10:44:52 +0800

    *

---
+++ map/components/PointLayer.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class PointLayer extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            pointId: ''
+        }
+    }
+    componentDidMount(){
+        this.getLayerInfoList();
+    }
+    getLayerInfoList(){
+        let id = 'point_186';
+        this.setState({
+            pointId: id
+        }, () => {
+            this.setLayerInfoList();
+        });
+    }
+    setLayerInfoList(){
+        const {pointId} = this.state;
+        console.log(pointId);
+    }
+    render(){
+        
+    }
+};
+
+export default PointLayer;
