9b7635128e4c2a4ba66c95d52cecfbcf9b747b1d
lijing <muclxylijing188@126.com>
2020-08-21 11:34:51 +0800

    utils.js

---
+++ utils/utils.js
@@ -0,0 +1,24 @@
+//获取地址栏参数
+let getParameter = (param) => {
+    var re = new RegExp("(^|&)" + param + "=([^&]*)(&|$)");
+    var result = window.location.search.substr(1).match(re);
+    return (result == null) ? null : unescape(result[2]);
+}
+
+let randomString = (len) => {
+    if (!len) {
+        len = 32;
+    }
+    var $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz';
+    var size = $chars.length;
+    var str = '';
+    for (var i = 0; i < size; i++) {
+        str += $chars.charAt(Math.floor(Math.random() * size));
+    }
+    return str;
+}
+
+export {
+    getParameter,
+    randomString
+}
