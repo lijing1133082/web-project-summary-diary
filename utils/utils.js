bbe9c0b31d0a6f70c48a0af86a8eb6a0776ca5d5
lijing <muclxylijing188@126.com>
2020-08-25 09:58:52 +0800

    *

--- utils/utils.js
+++ utils/utils.js
@@ -20,9 +20,9 @@ let getParameter = (param) => {
 }
 
 let getRootPath = () => {
-	var pathName = window.document.location.pathname
-	var projectName = pathName.substring(0, pathName.substr(1).indexOf('/') + 1)
-	return projectName
+	var pathName = window.document.location.pathname;
+	var projectName = pathName.substring(0, pathName.substr(1).indexOf('/') + 1);
+	return projectName;
 }
 
 export {
