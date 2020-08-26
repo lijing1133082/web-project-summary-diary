37298d88fe46a0c2f83b4ec15edcce8520b78baf
lijing <muclxylijing188@126.com>
2020-08-26 20:33:45 +0800

    *

---
+++ utils/slider.js
@@ -0,0 +1,14 @@
+export const slider = {
+    index_statistical_analysis: [
+        {
+			menuName: '日活情况',
+			url: 'IndexDailyDctivity',
+			list: []
+        },
+        {
+            menuName: '日活情况（新）',
+			url: 'IndexDailyDctivity2',
+			list: []
+        }
+    ]
+}
